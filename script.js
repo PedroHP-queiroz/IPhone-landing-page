4b36f2a1f76b6239dc481b60a00d709b22b2ba68
PEDRO HENRIQUE PEREIRA DE QUEIROZ <101682986+PedroHP-queiroz@users.noreply.github.com>
2022-12-11 17:17:44 -0300

    upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Func telefone
+function imgSlider(ev){
+    document.querySelector('#phone').src = ev
+}
+// Func circle
+function circleChange(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color
+}
